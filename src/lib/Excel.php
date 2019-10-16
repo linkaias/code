3aaa2f9e67a9b583ccba00516d8dd0af6ea926f8
lk <18337180580@163.com>
2019-10-16 14:35:08 +0800

    1

---
+++ src/lib/Excel.php
@@ -0,0 +1,141 @@
+<?php
+namespace lkcodes\Mycode\lib;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\Writer\Exception;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+/**
+ * Class Excel
+ * @package lkcodes\Mycode\lib
+ * by lk
+ */
+class Excel{
+
+    public function __construct()
+    {
+        set_time_limit(0);
+    }
+
+    /**
+     * 导出表格
+     * @param $data
+     * @param $params
+     *          $param=[
+                    'name'=>'风险广告列表',
+                    'suffix'=>'xlsl',
+                    'top_title'=>['ID','姓名','age'],
+                    'title'=>'title',
+                    'data_title'=>['test1','test2','test3'],
+                    'description'=>'文件描述',
+                    'keywords'=>'关键词',
+                    'category'=>'分类',
+                    ];
+     * @return string
+     */
+    public function exportData($data,$params){
+        $param=[
+            'name'=>isset($params['name'])?$params['name']:'测试',
+            'suffix'=>isset($params['suffix'])?$params['suffix']:'xlsl',
+            'top_title'=>isset($params['top_title'])?$params['top_title']:[],
+            'title'=>isset($params['title'])?$params['title']:'title',
+            'data_title'=>isset($params['data_title'])?$params['data_title']:[],
+            'description'=>isset($params['description'])?$params['description']:'文件描述',
+            'keywords'=>isset($params['keywords'])?$params['keywords']:'关键词',
+            'category'=>isset($params['category'])?$params['category']:'分类',
+        ];
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->getProperties()
+            ->setCreator("lk")//设置作者
+            ->setLastModifiedBy("lk")//最后修改
+            ->setTitle($param['title'])//设置标题
+            ->setDescription($param['description'])//设置文件描述
+            ->setKeywords($param['keywords'])//关键词
+            ->setCategory($param['category']);
+
+        //规格设定
+        $num ='A';
+        foreach ($param['top_title'] as $title){
+            $_num = $num.'1';
+            try {
+                //设置首行标题
+                $spreadsheet->getActiveSheet()->setCellValue($_num, $title);
+                //自动设置列宽度
+                $spreadsheet->getActiveSheet()->getColumnDimension($num)->setAutoSize(true);
+                //设置自动换行'\n'
+                $spreadsheet->getActiveSheet()->getStyle($_num)->getAlignment()->setWrapText(true);
+
+            } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            }
+            $num = Tool::chrNext($num);
+        }
+
+        //数据写入
+        $a=1;
+        foreach ($data as $k) {
+            $a++;
+            $num ='A';
+            foreach($param['data_title'] as $title){
+                try {
+                    $spreadsheet->getActiveSheet()->setCellValue($num.$a, $k[$title]);
+                } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+
+                }
+                $num = Tool::chrNext($num);
+            }
+        }
+        try {
+            $this->saveTableType($param['name'], $param['suffix'], $spreadsheet);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        //合并单元格
+        //$spreadsheet->getActiveSheet()->mergeCells('A2:A3');
+        //$spreadsheet->getActiveSheet()->unmergeCells('A2:A3');
+
+
+        //释放内存
+        $spreadsheet->disconnectWorksheets();
+        unset($spreadsheet);
+        die;
+
+
+
+    }
+    
+    /**
+     * 根据类型导出文件
+     * @param $name
+     * @param $type
+     * @param $obj
+     * @throws Exception
+     */
+    protected function saveTableType($name,$type,$obj)
+    {
+        switch ($type){
+            case 'xlsl':
+                $writer = new Xlsx($obj);
+                $writer->save($name.'.xlsx');
+                break;
+            case 'csv':
+                $writer = new Csv($obj);
+                $writer->save($name.'.csv');
+                break;
+            case 'xls':
+                $writer = new Xls($obj);
+                $writer->save($name.'.xls');
+                break;
+            case 'web': //输出到浏览器
+                $writer = new Xlsx($obj);
+                $writer->save('php://output');
+                break;
+        }
+        exit();
+
+    }
+
+
+}
--- test.php
+++ test.php
@@ -1,7 +1,126 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Laurentvw\Scrapher\Scrapher;
+use Laurentvw\Scrapher\Selectors\RegexSelector;
 use lkcodes\Mycode\lib\DB;
+use lkcodes\Mycode\lib\Excel;
 use lkcodes\Mycode\lib\Tool;
+
+$data=[
+    ['test1'=>'test1111','test2'=>'test222','test3'=>'test333'],
+    ['test1'=>'test1111','test2'=>'test222','test3'=>'test333'],
+    ['test1'=>'test1111','test2'=>'test222','test3'=>'test333'],
+];
+$param=[
+    'name'=>'风险广告列表',
+    'suffix'=>'xlsl',
+    'top_title'=>['T1','T2','T3'],
+    'title'=>'title',
+    'data_title'=>['test1','test2','test3'],
+    'description'=>'文件描述',
+    'keywords'=>'关键词',
+    'category'=>'分类',
+];
+$a = (new Excel())->exportData($data,$param);
+
+
+die;
+
+
+$url = 'https://cn.vuejs.org/v2/guide/';
+
+//$res = QueryList::get('https://www.layui.com/doc/element/table.html')->find('cite')->getString();
+$res = QueryList::get('https://blog.csdn.net/my_yang/article/details/43882661')
+    ->find('#asideArchive')->children('.aside-content')->children('ul')->children('li')
+    ->find('a')
+    ->getString();
+var_dump($res);die;
+
+/*bin/magento setup:install --base-url=http://mg29.cn --db-host=localhost --db-name=mg29 --db-user=root --db-password=root --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password=123456abc --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1*/
+
+
+$ql->find('#head')->append('<div>Append content</div>')->find('div')->htmls();
+$ql->find('.two')->children('img')->attrs('alt'); // Get the class is the "two" element under all img child nodes
+// Loop class is the "two" element under all child nodes
+$data = $ql->find('.two')->children()->map(function ($item){
+    // Use "is" to determine the node type
+    if($item->is('a')){
+        return $item->text();
+    }elseif($item->is('img'))
+    {
+        return $item->alt;
+    }
+});
+
+
+
+$pattern = '/http:\/\/.*com/';//需要转义/
+
+preg_match($pattern,$url,$match);
+
+var_dump($match);
+
+$str = 'http://baidu.com';
+
+$pattern = '#http://.*com#';//不需要转义/
+
+preg_match($pattern,$str,$match);
+
+var_dump( $match);
+die;
+
+$scrapher = new Scrapher($url);
+
+// Match all links on a page
+$regex = '/<a.*?href=(?:"(.*?)").*?>(.*?)<\/a>/ms';
+
+$matchConfig = array(
+    array(
+        'name' => 'url',
+        'id' => 1, // the first match (.*?) from the regex
+    ),
+    array(
+        'name' => 'title',
+        'id' => 2, // the second match (.*?) from the regex
+    ),
+);
+
+
+$matches = $scrapher->with(new RegexSelector($regex, $matchConfig));
+
+$results = $matches->get();
+print_r($results);
+die;
+
+
+
+$data = QueryList::get('https://cn.vuejs.org/v2/guide/');
+
+
+$res =QueryList::get('https://shimo.im/sheet/4wu1FFUVsc8PxJ2h/RIDOC')
+    ->rules([
+        'title'=>array('span','text'),
+        'link'=>array('h3>a','href')
+    ])
+    ->query()
+    ->getData();
+print_r($res);die;
+
+$data = QueryList::get('https://cn.vuejs.org/v2/guide/')
+    // 设置采集规则
+    ->rules([
+        'title'=>array('h3','text'),
+        'link'=>array('h3>a','href')
+    ])
+    ->query()->getData();
+
+
+print_r($data->all());
+
+
+
+
 die;
 
 
